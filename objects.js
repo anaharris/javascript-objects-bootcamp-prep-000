39ee86b37f2dd43d8a7e4fad357f12bbae0e24c3
Ana Harris <vana1990@gmail.com>
2018-09-06 20:23:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle.string;
+  playlist.artistName = songTitle;
   return playlist;
 }
 
